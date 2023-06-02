b3c5c466f6c5842be8f92ac9a7d19ca94e0cce48
peter offodile <peteroffodile@gmail.com>
2023-06-02 06:10:07 +0100

    property type front end functionalities

--- app/Http/Controllers/PropertyTypeController.php
+++ app/Http/Controllers/PropertyTypeController.php
@@ -12,9 +12,10 @@ class PropertyTypeController extends Controller
 {
     use ApiResponse;
 
-    /**
+     /**
      * @OA\Get(
      *      path="/property-type",
+     *      operationId="getPropertyTypeList",
      *      tags={"Property Type"},
      *      summary="List of property type",
      *      description="Returns list of property types",
@@ -22,11 +23,11 @@ class PropertyTypeController extends Controller
      *      @OA\Response(
      *          response=200,
      *          description="Successful operation",
-     *          
+     *          @OA\JsonContent(ref="#/components/schemas/PropertyTypeResource")
      *       ),
      *      @OA\Response(
      *          response=401,
-     *          description="Unauthorized",
+     *          description="Unauthenticated",
      *      ),
      *      @OA\Response(
      *          response=403,
@@ -72,40 +73,120 @@ class PropertyTypeController extends Controller
      */
     public function store(PropertyTypeRequest $request)
     {
-        // $data = $request->validated();
         try {
             $propertyType = PropertyType::create($request->validated());
-            if ($propertyType) {
-                return $this->success(new PropertyTypeResource($propertyType), "Property types created successfully", 200);
-            }
+            return $this->success(new PropertyTypeResource($propertyType), "Property types created successfully", 200);
         } catch (\Throwable $th) {
-            return $th;
+            // return $th;
+            return $this->error($th, 400);
         }
 
-        // return $this->error("", 400);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *      path="/property-type/{id}",
+     *      operationId="updateProperyType",
+     *      tags={"Property Type"},
+     *      summary="Update existing property type",
+     *      description="Returns updated property type data",
+     *      security={ {"bearer_token": {} }},
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Property type id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/PropertyTypeRequest")
+     *      ),
+     *      @OA\Response(
+     *          response=202,
+     *          description="Successful operation",
+     *          @OA\JsonContent(ref="#/components/schemas/PropertyType")
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
      */
-    public function update(Request $request, string $id)
+    public function update(PropertyTypeRequest $request, PropertyType $propertyType)
     {
-        //
+        try {
+            $propertyType->update($request->validated());
+            return $this->success(new PropertyTypeResource($propertyType), "property type updated", 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return $this->error($th, 400);
+        }
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *      path="/property-type/{id}",
+     *      operationId="deleteProperyType",
+     *      tags={"Property Type"},
+     *      summary="delete existing property type",
+     *      description="Deletes record and returns no content",
+     *      security={ {"bearer_token": {} }},
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=204,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
      */
-    public function destroy(string $id)
+    public function destroy(PropertyType $propertyType)
     {
-        //
+        try {
+            $propertyType->delete();
+            return $this->success("", "property type deleted", 200);
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage(), 400);
+        }
+        
     }
 }
--- react/src/pages/PropertyType/CreatePropertyType.jsx
+++ react/src/pages/PropertyType/CreatePropertyType.jsx
@@ -1,13 +1,109 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Breadcrumb from '../../components/Breadcrumb'
 import { PencilSquareIcon, TrashIcon, ExclamationTriangleIcon, CreditCardIcon, CheckBadgeIcon, XMarkIcon, EyeIcon } from '@heroicons/react/24/outline';
+import axiosInstance from '../../axios.js';
+import { toast } from 'react-toastify'
+import Loader from '../../components/Loader/Loader';
 
 const CreatePropertyType = () => {
+
+    const [propertyTypes, setPropertyTypes] = useState([])
+    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState({
+        name: "",
+        description: ""
+    })
+
+    useEffect(() => {
+        getPropertyTypes()
+    }, [])
+
+    const getPropertyTypes = async () => {
+        await axiosInstance.get(`property-type`)
+            .then(({ data }) => {
+                console.log(data)
+                setPropertyTypes(data.data)
+            }).catch((err) => {
+                console.log(err)
+            })
+    }
+
+    const handleSave = async (e) => {
+        e.preventDefault()
+        console.log("data", data)
+        setLoading(true)
+        if(data.id){
+            await axiosInstance.put(`property-type/${data.id}`, data)
+            .then(({ data }) => {
+                setLoading(false)
+                toast.success(data.message)
+                const updatedList = propertyTypes.map((type) => {
+                    if(type.id === data.data.id){
+                        const updated = {
+                            ...type,
+                            name: data.data.name,
+                            description: data.data.description
+                        }
+                        return updated
+                    }
+                    return type
+                })
+                setPropertyTypes(updatedList)
+                setData({
+                    id: "",
+                    name: "",
+                    description: ""
+                })
+            }).catch((err) => {
+                setLoading(false)
+                console.log(err)
+            })
+        }else{
+            await axiosInstance.post(`property-type`, data)
+            .then(({ data }) => {
+                setLoading(false)
+                toast.success(data.message)
+                const newTypes = [...propertyTypes]
+                newTypes.push(data.data)
+                setPropertyTypes(newTypes)
+                setData({
+                    name: "",
+                    description: ""
+                })
+            }).catch((err) => {
+                setLoading(false)
+                console.log(err)
+            })
+        }
+        
+    }
+
+    const handleUpdate = async(pType) => {
+        setData({
+            id: pType.id,
+            name: pType.name,
+            description: pType.description
+        })
+    }
+
+    const handleRemove = async (id) => {
+        setLoading(true)
+        await axiosInstance.delete(`property-type/${id}`)
+            .then((res) => {
+                setLoading(false)
+                toast.success(res.data.message)
+                setPropertyTypes(propertyTypes.filter((type) => type.id !== id))
+            }).catch((err) => {
+                setLoading(false)
+                console.log(err)
+            })
+    }
+
     return (
         <>
             <Breadcrumb pageName="Property Type" />
 
-            <div className="flex flex-col md:flex-row gap-9">
+            <div className="flex flex-col-reverse md:flex-row gap-6">
                 <div className="md:w-1/2">
                     {/* <!-- Contact Form --> */}
                     <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
@@ -16,7 +112,7 @@ const CreatePropertyType = () => {
                                 Create property type
                             </h3>
                         </div>
-                        <form action="#">
+                        <form onSubmit={handleSave}>
                             <div className="p-6.5">
 
                                 <div className="mb-4.5">
@@ -27,6 +123,8 @@ const CreatePropertyType = () => {
                                         type="text"
                                         placeholder="property type"
                                         className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                                        value={data.name}
+                                        onChange={(e) => setData({ ...data, name: e.target.value })}
                                     />
                                 </div>
 
@@ -38,16 +136,19 @@ const CreatePropertyType = () => {
                                         type="text"
                                         placeholder="description"
                                         className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                                        value={data.description}
+                                        onChange={(e) => setData({ ...data, description: e.target.value })}
                                     />
                                 </div>
 
-                                <button className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray">
+                                <button type='submit' className="flex w-full justify-center rounded bg-primary p-3 font-medium text-gray">
                                     Save
                                 </button>
                             </div>
                         </form>
                     </div>
                 </div>
+
 
                 <div className="md:w-1/2">
                     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
@@ -66,31 +167,42 @@ const CreatePropertyType = () => {
                                         </th>
                                     </tr>
                                 </thead>
-                                <tbody>
-                                    <tr>
-                                        <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
-                                            <h5 className="font-medium text-black dark:text-white">
-                                                Bungalow
-                                            </h5>
-                                        </td>
-                                        <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                            <p className="text-black dark:text-white">Well buit bungalow</p>
-                                        </td>
-                                        <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
-                                            <div className="flex items-center space-x-3.5">
-                                                <button className="hover:text-primary">
-                                                    <PencilSquareIcon className='w-6 h-6 text-success' />
-                                                    
-                                                </button>
-                                                <button className="hover:text-primary">
-                                                    <TrashIcon className='w-6 h-6 text-danger' />
-                                                </button>
-                                                
-                                            </div>
-                                        </td>
-                                    </tr>
-                                    
-                                </tbody>
+                                {loading && <Loader />}
+                                {!loading &&
+                                    <tbody>
+                                        {propertyTypes && propertyTypes.map((pType) => (
+                                            <tr>
+                                                <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
+                                                    <h5 className="font-medium text-black dark:text-white">
+                                                        {pType.name}
+                                                    </h5>
+                                                </td>
+                                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                                                    <p className="text-black dark:text-white">{pType.description}</p>
+                                                </td>
+                                                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                                                    <div className="flex items-center space-x-3.5">
+                                                        <button className="hover:text-primary">
+                                                            <PencilSquareIcon 
+                                                                onClick={() => handleUpdate(pType)}
+                                                                className='w-6 h-6 text-success' 
+                                                            />
+
+                                                        </button>
+                                                        <button className="hover:text-primary">
+                                                            <TrashIcon
+                                                                onClick={() => handleRemove(pType.id)}
+                                                                className='w-6 h-6 text-danger'
+                                                            />
+                                                        </button>
+
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        ))}
+
+                                    </tbody>
+                                }
                             </table>
                         </div>
                     </div>
